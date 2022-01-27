0b1ab4922a16c6955a86e4b5000158a9382b1ed6
samuel <samuelczhu@gmail.com>
2022-01-27 18:29:28 -0500

    Added functionalities to simulate multiple particles

--- ElectronSim1D.m
+++ ElectronSim1D.m
@@ -16,55 +16,56 @@ C.m_0 = 9.10938215e-31;             % electron mass
 
 % Simulation status
 deltaTime = 1e-5; % Change in time per simulation step (in second)
-pauseTime = 0.5; % Pause time per simulation step for the graph (in second)
+pauseTime = 0.2; % Pause time per simulation step for the graph (in second)
 nSim = 1000; % Number of simulations
+nParticles = 3; % Number of particle
 
 % Electrons status
-E = 0.1; % electric field (V/m)
+E = 1e-6; % electric field (V/m)
 F = C.q_0*E; % force on the electron: F=qE
-x = zeros(1,nSim); % position array of the electron
-v = zeros(1,nSim); % velocity array of the electron
-driftV = zeros(1,nSim); % array for the drift velocity
+x = zeros(nParticles,nSim); % position matrix of the electron
+v = zeros(nParticles,nSim); % velocity matrix of the electron
 t = zeros(1,nSim); % Time array
 
 % Loop for simulation
-for index = 1:nSim
-    % Plot position vs time
-    subplot(2,1,1);
-    plot(t(1:index), x(1:index)); 
-    xlabel("t")
-    ylabel("x")
-    % Plot velocity vs time
-    subplot(2,1,2);
-    plot(t(1:index), v(1:index));
-    xlabel("t")
-    ylabel("v")
-    % Plot drift velocity vs time
-    plot(t(1:index), driftV(1:index), "-bo");
-    hold on
+for indexSim = 1:nSim
+    % Loop for particle
+    for indexParticle = 1:nParticles
+        % Plot position vs time
+        subplot(2,1,1);
+        plot(t(1, 1:indexSim), x(indexParticle, 1:indexSim)); 
+        xlabel("t")
+        ylabel("x")
+        hold on
+        % Plot velocity vs time
+        subplot(2,1,2);
+        plot(t(1, 1:indexSim), v(indexParticle, 1:indexSim));
+        xlabel("t")
+        ylabel("v")
+        hold on
 
-    % Increment time
-    t(index+1) = t(index) + deltaTime;
-    % Calculate the acceleration F=ma => a = F/m
-    a = F/C.m_0;
+        % Increment time
+        t(1, indexSim+1) = t(1, indexSim) + deltaTime;
+        % Calculate the acceleration F=ma => a = F/m
+        a = F/C.m_0;
 
-    % Check for random
-    if rand() <= 0.05
-        v(index+1) = -0.25*v(index);
-    else
-        % Update the velocity
-        v(index+1) = v(index) + a*deltaTime;
+        % Check for random
+        if rand() <= 0.05
+            v(indexParticle, indexSim+1) = -0.25*v(indexParticle, indexSim);
+        else
+            % Update the velocity
+            v(indexParticle, indexSim+1) = v(indexParticle, indexSim) + a*deltaTime;
+        end
+
+        % Update the position
+        x(indexParticle, indexSim+1) = x(indexParticle, indexSim) + v(indexParticle, indexSim)*deltaTime;
     end
-
-    % Update the position
-    x(index+1) = x(index) + v(index)*deltaTime;
 
     % Calculate the drift velocity
-    sum_v = 0; % Hold the sum of the velocity so far
-    for d = 1:index
-        sum_v = sum_v + v(d);
-    end
-    driftV(index+1) = sum_v/index; % drift velocity = average velocity
+    sum_v = sum(v, "all"); % sum of all of the velocity so far
+    driftV = sum_v/(indexSim * nParticles); % drift velocity = average velocity
+    % Display the drift velocity as title
+    title("Drift Velocity:"+driftV)
     
     % Pause for some time
     pause(pauseTime)
